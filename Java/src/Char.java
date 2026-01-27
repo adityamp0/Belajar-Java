3fcf995d2fc7cad0e1597ae23df8f58c743ac4dd
adityamp0 <ampaditya4@gmail.com>
2026-01-27 16:40:47 +0700

    Belajar Java

--- Java/src/Char.java
+++ Java/src/Char.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Char {
+    static void main() {
+        char a = 'A';
+        char d = 'D';
+        char i = 'I';
+        char t = 'T';
+
+        System.out.println(a);
+        System.out.println(d);
+        System.out.println(i);
+        System.out.println(t);
+    }
 }
--- Java/src/KonversiTipeDataNumber.java
+++ Java/src/KonversiTipeDataNumber.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class KonversiTipeDataNumber {
+    static void main() {
+        byte iniByte = 100;
+        short iniShort = iniByte;
+        int iniInt = iniShort;
+
+        int iniInt2 = 1000;
+        byte iniByte2 = (byte) iniInt2;
+
+        long iniLong = 1000000000;
+        long iniLong2 = 10000000000L;
+
+        float iniFloat = 10.10F;
+        double iniDouble = 10.10;
+
+        int decimalInt = 34;
+        int hexDecimal = 0xFFFFFF;
+        int binaryDecimal = 0b10101010;
+
+        int amount = 1_000_000_000;
+    }
 }
